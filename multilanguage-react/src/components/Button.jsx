60d98dfd3c169fe55019adf3a4862882b003682f
victorGVC <victorstudy17@hotmail.com>
2022-03-17 07:56:20 -0300

    update: Button is now better managed with variables

--- multilanguage-react/src/components/Button.jsx
+++ multilanguage-react/src/components/Button.jsx
@@ -1,13 +1,21 @@
 import { ButtonStyle } from "./style/Button"
+import Ripple from 'material-ripple-effects';
 
 export function Button(props) {
+    const ripple = new Ripple();
 
-    const { type = 'none', text = 'OK', 
-        radius = '25' } = props
+    const { type = 'none', text = 'OK',
+        radius = '25', 
+        width = '220'} = props
 
     return (
-        <ButtonStyle radius={radius} type={type}>
+        <ButtonStyle onMouseDown={(e) => ripple.create(e, 'dark')} 
+            radius={radius} 
+            type={type}
+            width={width} >
             {text}
         </ButtonStyle>
     )
 }
+
+export default Button
